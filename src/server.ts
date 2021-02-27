6b0d2669d1d56a9b57b045bf135487f7cda2c437
Luiz Pedro <luizpsmoreira@gmail.com>
2021-02-27 16:48:40 -0300

    NLW4 Day2

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,10 @@
+import "reflect-metadata";
 import express from "express";
+import "./database"
+import { router } from "./routes";
 
 const app = express();
-
-/**
- * GET => Busca
- * POST => Salvar
- * PUT => Alterar
- * DELETE => Deletar
- * PATCH => Alteração específica
- */
-
-// http://localhost:3333/
-app.get("/", (request, response) => {
-  //   return response.send("Hello World NLW 4");
-  return response.json({ message: "Hello World - NLW 4" });
-});
-
-// 1 param => Rota(Recurso API)
-// 2 param => request, response
-app.post("/", (request, response) => {
-  //   Recebeu os dados para salvar
-  return response.json({ message: "Os dados foram salvos com sucesso!" });
-});
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, () => console.log("Server is running!"));
